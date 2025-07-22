c78c9f21f2e38ad6d9e3c5af6a2a621b552c9c91
NitinTSutar <nitintsuthar67@gmail.com>
2025-07-22 23:25:58 +0530

    created user/signup API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 const express = require("express");
-const {connectDB} = require("./configs/database");
+const { connectDB } = require("./configs/database");
+const User = require("./models/user");
 const app = express();
+
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Nitin",
+        lastName: "Suthar",
+        email: "nitin.suthar@example.com",
+        password: "password123",
+        age: 25,
+        gender: "male",
+    });
+
+    try {
+        await user.save();
+        res.send("User created successfully");
+    } catch (err) {
+        res.status(400).send("user not created", err.message);
+    }
+});
 
 connectDB()
     .then(() => {
--- src/configs/database.js
+++ src/configs/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://nitintsuthar67:A9DXBW69LZHbZWRx@cluster0.zpknzf4.mongodb.net/"
+        "mongodb+srv://nitintsuthar67:A9DXBW69LZHbZWRx@cluster0.zpknzf4.mongodb.net/devTinder"
     );
 };
 
