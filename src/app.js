e790ab498f181ee9cf291dd61b0933852f9aa31e
NitinTSutar <nitintsuthar67@gmail.com>
2025-07-23 21:30:27 +0530

    getUSer by id api & feed api

--- src/app.js
+++ src/app.js
@@ -16,6 +16,35 @@ app.post("/signup", async (req, res) => {
     }
 });
 
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email;
+    try {
+        const users = await User.find({
+            email: userEmail,
+        });
+        if(users.length === 0 ){
+            res.status(404).send("User not found")
+        }else{
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong", err.message);
+    }
+});
+
+app.get("/feed", async (req, res) =>{
+    try{
+        const users = await User.find({})
+        if(users.length === 0 ){
+            res.status(404).send("User not found")
+        }else{
+            res.send(users);
+        }
+    }catch (err){
+        res.status(400).send("Something went wrong", err.message);
+    }
+})
+
 connectDB()
     .then(() => {
         console.log("connected to DB");
