ab6c84527a09227db46e6d38e44c3f207d6687fa
NitinTSutar <nitintsuthar67@gmail.com>
2025-07-26 15:41:22 +0530

    Implement password update functionality with strong password validation; add connection request model

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const profileRouter = express.Router();
 const { validateEditProfileData } = require("../utils/validation");
+const validator = require("validator");
+const User = require("../models/user");
 
 // User Profile
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
@@ -40,5 +42,28 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     }
 });
 
-module.exports = profileRouter;
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+    try {
+        const { password } = req.body;
 
+        if (!validator.isStrongPassword(password)) {
+            throw new Error("New password is not strong enough");
+        }
+        const bcrypt = require("bcrypt");
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = req.user;
+        user.password = hashedPassword;
+        await user.save();
+
+        res.send("password updated successfuly");
+        // res.json({
+        //     message: "$(User.firstName), Your Password Updated successfuly",
+        //     data: pass,
+        // });
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+module.exports = profileRouter;
